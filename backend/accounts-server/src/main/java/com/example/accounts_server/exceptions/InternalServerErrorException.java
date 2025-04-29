3c487f7d1eab888d5fb29eb469514b95b8535414
Herman Schubert <dochronosplus@gmail.com>
2025-04-29 14:58:36 -0300

    Refactor: mejora en InternalServerErrorException con mensaje por defecto y javadoc.

--- backend/accounts-server/src/main/java/com/example/accounts_server/exceptions/InternalServerErrorException.java
+++ backend/accounts-server/src/main/java/com/example/accounts_server/exceptions/InternalServerErrorException.java
@@ -3,15 +3,14 @@ package com.example.accounts_server.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-public class InternalServerErrorException extends RuntimeException{
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerErrorException extends RuntimeException {
 
     public InternalServerErrorException() {
-        super();
+        super("Internal server error");
     }
 
     public InternalServerErrorException(String message) {
         super(message);
     }
 }
-
