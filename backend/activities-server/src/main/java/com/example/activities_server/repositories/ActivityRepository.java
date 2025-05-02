de8ba3d824aade9a5bea04e070fc7531d16c5d37
Herman Schubert <dochronosplus@gmail.com>
2025-05-02 12:51:28 -0300

    refactor(activities-server): optimiza definición de ActivityRepository.
    - Se mejora el orden y formato del repositorio para mayor legibilidad.
    - Se renombra el parámetro 'alias' a 'origin' en el método findTop5ByOriginOrderByDateDesc para mantener coherencia con el nombre real del atributo en la entidad 'Activity'.

--- backend/activities-server/src/main/java/com/example/activities_server/repositories/ActivityRepository.java
+++ backend/activities-server/src/main/java/com/example/activities_server/repositories/ActivityRepository.java
@@ -9,10 +9,12 @@ import java.util.Optional;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
     List<Activity> findTop5ByUserIdOrderByDateDesc(Long userId);
+
     List<Activity> findAllByUserIdOrderByDateDesc(Long userId);
 
     Optional<Activity> findByUserIdAndId(Long userId, Long id);
-    List<Activity> findTop5ByOriginOrderByDateDesc(String alias);
 
+    List<Activity> findTop5ByOriginOrderByDateDesc(String origin);
 }
