5bd5b6f9c1686416bf57ffbecf9fdd8c07b8f88e
Herman Schubert <dochronosplus@gmail.com>
2025-05-05 15:49:07 -0300

    refactor(users-server): cambia tipo de ID en UserDTO de String a Long para mayor coherencia con la entidad.

--- backend/users-server/src/main/java/com/example/users_server/dto/UserDTO.java
+++ backend/users-server/src/main/java/com/example/users_server/dto/UserDTO.java
@@ -9,16 +9,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
-    private String id;
+    private Long id;
     private String firstName;
     private String lastName;
     private String dni;
     private String email;
     private String phone;
 
-
     public UserDTO(User user) {
-        this.id = user.getId().toString();
+        this.id = user.getId();
         this.firstName = user.getFirstName();
         this.lastName = user.getLastName();
         this.dni = user.getDni();
